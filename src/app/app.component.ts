5a110a5dd7575f3f60e8d236596571df84a5ee8e
panosangel <panagiotis@angelidakis.net>
2022-06-21 17:27:13 +0300

    App Component / Use store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { StoreService } from '../store/store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-state-management';
+export class AppComponent implements OnInit {
+  constructor(
+    protected store: StoreService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.store.order.updateOrderDetails({
+      id: 'SKU.1234',
+      title: 'Brave New World',
+      description: 'What a come-back for the Maidens!'
+    });
+  }
 }
